c700a6bed71dbfed555299c98d527d4f40e47ef8
indoderiano <mde50526@gmail.com>
2020-11-28 12:54:56 +0700

    fixed mail verification

--- api/supports/ApiUrl.js
+++ api/supports/ApiUrl.js
@@ -1,2 +1,7 @@
 // export const APIURL='http://localhost:5000'
-export const APIURL='https://popstore-indo.herokuapp.com'
+// export const APIURL='https://popstore-indo.herokuapp.com'
+
+
+module.exports = {
+    APIURL='https://popstore-indo.herokuapp.com'
+}
